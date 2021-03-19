37022cebce93c671543fbb11791a1cd1af188d72
moisesRadix <moises@radixhaven.com>
2021-03-18 22:21:49 -0600

    modifing login view

--- src/views/auth/login/backgroundLogin.js
+++ src/views/auth/login/backgroundLogin.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import styled from 'styled-components';
-import '../../../assets/extra/loginbg.scss';
+import React from "react";
+import styled from "styled-components";
+import "../../../assets/extra/loginbg.scss";
 
 const BackgroundLogin = styled.div``;
 
 const BackgroundSpace = (props) => {
-	return (
-		<BackgroundLogin id='stars-container'>
-			<div class='page-bg'></div>
-			<div class='animation-wrapper'>
-				<div class='particle particle-1'></div>
-				<div class='particle particle-2'></div>
-				<div class='particle particle-3'></div>
-				<div class='particle particle-4'></div>
-			</div>
-			{props.children}
-		</BackgroundLogin>
-	);
+  return (
+    <BackgroundLogin id="stars-container">
+      <div className="page-bg"></div>
+      <div className="animation-wrapper">
+        <div className="particle particle-1"></div>
+        <div className="particle particle-2"></div>
+        <div className="particle particle-3"></div>
+        <div className="particle particle-4"></div>
+      </div>
+      {props.children}
+    </BackgroundLogin>
+  );
 };
 
 export default BackgroundSpace;
